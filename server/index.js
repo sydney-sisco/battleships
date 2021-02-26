2a2f12bbd5243eb0c9561136fea6562d6eed5763
Syd Sisco <hello@sydsis.co>
2021-02-26 15:06:55 -0800

    initial project setup

---
+++ server/index.js
@@ -0,0 +1,13 @@
+// Basic express setup:
+
+const PORT          = 8080;
+const express       = require("express");
+const bodyParser    = require("body-parser");
+const app           = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log("Example app listening on port " + PORT);
+});
