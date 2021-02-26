6d5b39367b01d752405616acbedcbca2eb86e929
Syd Sisco <hello@sydsis.co>
2021-02-26 15:31:19 -0800

    basic game board set up using JQuery

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,19 @@
+const BOARD_SIZE = 10;
+
 $(document).ready(function() {
   console.log('We\'re ready.');
+
+  createGameBoard($('.player'));
+  createGameBoard($('.opponent'));
 });
+
+
+const createGameBoard = ($element) => {
+
+  for(let i = 0; i < BOARD_SIZE; i++) {
+    for(let j = 0; j < BOARD_SIZE; j++) {
+      const $boardSquare = $('<div class="square"></div>').text(`${i}, ${j}`);
+      $($element).append($boardSquare);
+    }
+  }
+};
